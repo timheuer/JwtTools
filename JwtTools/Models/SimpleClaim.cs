64caefad939ee364771e023374e1027bf6e06784
Tim Heuer <tim@timheuer.com>
2022-05-27 15:45:53 -0700

    Added datagrid view explainer for payload [release]

---
+++ JwtTools/Models/SimpleClaim.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JwtTools.Models
+{
+    public class SimpleClaim
+    {
+        public string Type { get; set; }
+        public string Value { get; set; }
+        public string Description { get; set; }
+    }
+}
